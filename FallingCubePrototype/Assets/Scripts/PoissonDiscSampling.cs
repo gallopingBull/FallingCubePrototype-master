21e4ce01a0d3e17f98e69608c4df2c86d98a355f
Joshua <gallopingbull89@gmail.com>
2023-11-15 19:56:32 -0800

    added method to destroy and regenerate new set of cubes

--- FallingCubePrototype/Assets/Scripts/PoissonDiscSampling.cs
+++ FallingCubePrototype/Assets/Scripts/PoissonDiscSampling.cs
@@ -13,42 +13,59 @@ public class PoissonDiscSampling : MonoBehaviour
     public float spacing = 2f;
     public float floatProbability = 0.2f; // Probability of a cube floating
 
+    [SerializeField] List<GameObject> cubes;
+
     void Start()
     {
         SpawnTerrainWithFloatingCubes();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DestoryAllCubes();
+            SpawnTerrainWithFloatingCubes();
+        }
+    }   
+
     void SpawnTerrainWithFloatingCubes()
     {
-        Debug.Log($"Stepping into this shit");
+        cubes = new List<GameObject>();
+
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int z = 0; z < gridSizeZ; z++)
             {
                 int randomHeight = Random.Range(minHeight, maxHeight + 1);
-                // check if random height is an even number
+                // check if random height value is an even number, otherwise redo assignment
                 while (randomHeight % 2 != 0)
                 {
-
-                    Debug.Log($"randomHieght is at {randomHeight} is odd");
+                    Debug.Log($"randomHieght is odd - {randomHeight}");
                     randomHeight = Random.Range(minHeight, maxHeight + 1);
-                    //cubePosition.y += 1;
                 }
 
                 Vector3 cubePosition = new Vector3(x * spacing, randomHeight, z * spacing);
-
-             
 
                 // Check if the cube should float
                 if (Random.value < floatProbability)
                 {
                     float floatingHeight = maxHeight + Random.Range(1f, 5f); // Round to the nearest integer
                     cubePosition.y = floatingHeight;
-
-                    
                 }
 
-                Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+                cubes.Add(Instantiate(cubePrefab, cubePosition, Quaternion.identity));
+            }
+        }
+    }
+
+    private void DestoryAllCubes()
+    {
+        if (cubes.Count > 0)
+        {
+            foreach (GameObject cube in cubes)
+            {
+                Destroy(cube);
             }
         }
     }
