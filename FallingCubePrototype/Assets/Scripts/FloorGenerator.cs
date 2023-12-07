c98733ca6a718c96c19cafc53e842c2cf40ec99b
Joshua <gallopingbull89@gmail.com>
2023-12-06 18:39:01 -0800

    AHHHHHHH

--- FallingCubePrototype/Assets/Scripts/FloorGenerator.cs
+++ FallingCubePrototype/Assets/Scripts/FloorGenerator.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoissonDiscSampling : MonoBehaviour
+public class FloorGenerator : MonoBehaviour
 {
     public GameObject cubePrefab;
     public int gridSizeX = 10;
