97d7f620f8f41ce8c0d192a45cb4ae4fca92e820
Joshua <gallopingbull89@gmail.com>
2024-06-01 00:07:12 -0700

    removed extra space

--- FallingCubePrototype/Assets/Scripts/UIPanelFaceCamera.cs
+++ FallingCubePrototype/Assets/Scripts/UIPanelFaceCamera.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class UIPanelFaceCamera : MonoBehaviour
 {
-    
     void Start()
     {
         GetComponent<Canvas>().worldCamera = GameObject.Find("UICamera").GetComponent<Camera>();
