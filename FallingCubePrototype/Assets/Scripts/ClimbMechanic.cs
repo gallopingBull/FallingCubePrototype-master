004facfa5292f7619cd3e6948ae09785a3200f42
Joshua <9190823+gallopingBull@users.noreply.github.com>
2025-09-06 14:31:58 -0700

    adding linux trigger configuration

--- FallingCubePrototype/Assets/Scripts/ClimbMechanic.cs
+++ FallingCubePrototype/Assets/Scripts/ClimbMechanic.cs
@@ -3,9 +3,24 @@ using Invector.vCharacterController.vActions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClimbMechanic : vFreeClimb
 {
+    public Text text;
+    private string L_TriggerName = "LT";
+    private string R_TriggerName = "RT";
+
+    protected override void Start()
+    {
+        base.Start();
+        //text = GetComponent<Text>();
+#if UNITY_STANDALONE_LINUX && !UNITY_EDITOR
+        L_TriggerName = "LT_Linux";
+        R_TriggerName = "RT_Linux";
+#endif
+    }
+
     protected override void Update()
     {
         //Debug.Log($"inDrag: {dragInfo.inDrag}");    
@@ -14,14 +29,17 @@ public class ClimbMechanic : vFreeClimb
         if (GetComponent<MoveCubeMechanic>().IsMoving)
             return;
 
-        if (((Input.GetAxis("RT") == 1 && Input.GetAxis("LT") == 1) || Input.GetMouseButton(1)) && CheckClimbCondictions())
+        if (text != null)
+            text.text = $"{R_TriggerName} = {Input.GetAxis(R_TriggerName)}\n{L_TriggerName} = {Input.GetAxis(L_TriggerName)}";
+        
+        if (((Input.GetAxis(R_TriggerName) == 1 && Input.GetAxis(L_TriggerName) == 1) || Input.GetMouseButton(1)) && CheckClimbCondictions())
         {
             input = new Vector3(TP_Input.horizontalInput.GetAxis(), 0, TP_Input.verticallInput.GetAxis());
             ClimbHandle();
             ClimbJumpHandle();
             ClimbUpHandle();
         }
-        else if (((Input.GetAxis("RT") == 0 && Input.GetAxis("LT") == 0) || Input.GetMouseButtonUp(1)) && dragInfo.inDrag)
+        else if (((Input.GetAxis(R_TriggerName) == 0 && Input.GetAxis(L_TriggerName) == 0) || Input.GetMouseButtonUp(1)) && dragInfo.inDrag)
         {
             ExitClimb();
         }
